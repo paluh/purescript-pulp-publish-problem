76214a6e697325d82c8a770838f67dd23609505f
Tomasz Rybarczyk <paluho@gmail.com>
2018-03-01 17:03:00 +0100

    Only two comments left...

--- src/Main.purs
+++ src/Main.purs
@@ -1,6 +1,3 @@
 module Main where
 
-import Prelude
-
 -- | Test docs
-main = pure unit
--- test/Main.purs
+++ test/Main.purs
@@ -1,17 +1,3 @@
 module Test.Main where
 
-import Prelude
-import Control.Monad.Aff.AVar (AVAR)
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Timer (TIMER)
-import Control.Monad.Eff.Console (CONSOLE)
-import Test.Unit (suite)
-import Test.Unit.Console (TESTOUTPUT)
-import Test.Unit.Main (runTest)
-
-main :: forall eff. Eff ( timer :: TIMER
-                        , avar :: AVAR
-                        , console :: CONSOLE
-                        , testOutput :: TESTOUTPUT | eff ) Unit
-main = runTest $ do
-  pure unit
+-- | Some docs
